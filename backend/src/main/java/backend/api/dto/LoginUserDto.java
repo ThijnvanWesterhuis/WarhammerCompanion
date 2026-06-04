9204169ba79c845cd62415c7d21c67ca09c7f0ba
ThijnvanWesterhuis <thijn.westerhuis@gmail.com>
2026-06-04 19:52:33 +0200

    Account system with jwt

---
+++ backend/src/main/java/backend/api/dto/LoginUserDto.java
@@ -0,0 +1,16 @@
+package backend.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDto {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
